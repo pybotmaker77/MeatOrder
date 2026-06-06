8754a07ccfef55a7e0f0b73df71d6372bb1e24f5
pybotmaker77 <37127596+pybotmaker77@users.noreply.github.com>
2026-06-06 23:28:44 +0300

    Add files via upload

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,14 @@
-pluginManagement {
+﻿pluginManagement {
     repositories {
         google()
         gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
     }
 }
 rootProject.name = "MeatOrder"
