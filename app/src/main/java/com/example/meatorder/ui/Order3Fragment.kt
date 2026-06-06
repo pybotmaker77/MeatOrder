14e1779b7a1cad9bb30a4da392475887eb3a9868
pybotmaker77 <37127596+pybotmaker77@users.noreply.github.com>
2026-06-07 02:07:37 +0300

    Update Order3Fragment.kt

--- app/src/main/java/com/example/meatorder/ui/Order3Fragment.kt
+++ app/src/main/java/com/example/meatorder/ui/Order3Fragment.kt
@@ -5,8 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
-import com.example.meatorder.R
 import com.example.meatorder.databinding.FragmentOrder3Binding
 
 class Order3Fragment : Fragment() {
@@ -19,17 +17,6 @@ class Order3Fragment : Fragment() {
     ): View {
         _binding = FragmentOrder3Binding.inflate(inflater, container, false)
         return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.fabContinue.setOnClickListener {
-            val bundle = Bundle().apply {
-                putString("finalItemsJson", "Тестовый заказ")
-            }
-            findNavController().navigate(R.id.action_order3Fragment_to_order4Fragment, bundle)
-        }
     }
 
     override fun onDestroyView() {
