49b5ac884f41a8cb6864e7f6d570b20e1888a272
pybotmaker77 <37127596+pybotmaker77@users.noreply.github.com>
2026-06-08 20:40:15 +0300

    Update Order1Fragment.kt

--- app/src/main/java/com/example/meatorder/ui/Order1Fragment.kt
+++ app/src/main/java/com/example/meatorder/ui/Order1Fragment.kt
@@ -33,6 +33,7 @@ class Order1Fragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        applyFontSize(binding.root, getPrefs().fontSize)
 
         val header = binding.root.findViewById<androidx.appcompat.widget.Toolbar>(R.id.header)
         header?.setNavigationOnClickListener { findNavController().popBackStack() }
